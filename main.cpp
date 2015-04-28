e7cdd4f51a33e8ff6ba8bd054eaa36319e782810
corsel <orselcem@gmail.com>
2015-04-28 16:58:18 +0200

    haci naber?

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 
 int main()
 {
+	ScalarField torqueMap;
+	torqueMap.getDataFromFile("data/EngineMap.csv");
+
+	EngineProps engineProps(torqueMap, 1.0f);
+	Engine engine(engineProps);
+
+	std::vector<float> gearRatio;
+	gearRatio.push_back(-1.0f);
+	gearRatio.push_back(1.0f);
+	gearRatio.push_back(1.5f);
+	gearRatio.push_back(2.0f);
+	GearBoxProps gearBoxProps(gearRatio);
+	GearBox gearBox(gearBoxProps);
+
+	DifferentialProps differentialProps(0.5f);
+	Differential differential(differentialProps);
+
+	Powertrain powertrain(&engine, &gearBox, &differential);
+
 	int a; std::cin >> a;
 
 	return 0;
